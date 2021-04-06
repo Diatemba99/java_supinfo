4fa708a4ad74031150a622b8d78d94f9baf6b9e2
Diatemba99 <martin.diatta99@gmail.com>
2021-04-06 15:48:31 +0000

    adding new seance on TestSeanceFormation

--- javapoo/src/main/java/classesrelationship/TestSeanceFormation.java
+++ javapoo/src/main/java/classesrelationship/TestSeanceFormation.java
@@ -1,6 +1,8 @@
 package classesrelationship;
 
 public class TestSeanceFormation {
+    private static Object salleFormation;
+
     public static void  main(string [] args)
     {
         SalleFormation salleFormation1 = new SalleFormatiion();
@@ -8,14 +10,41 @@ public class TestSeanceFormation {
         salleFormation1.setNom("Blue-Ray");
         salleFormation1.capicite(30);
 
-        SeanceFormation seanceFormation = new SeanceFormation();
+        SeanceFormation seanceFormation1 = new SeanceFormation();
         seanceFormation1.setDate("06/04/2021");
         seanceFormation1.setHeureDebut("15h");
         seanceFormation1.setHeureFin("17h");
         seanceFormation1.setSalleFormation(salleFormation);
 
+        //====================================================
+        SalleFormation salleFormation2 = new SalleFormatiion();
+        salleFormation2.setId(1);
+        salleFormation2.setNom("Blue-Ray");
+        salleFormation2.capicite(30);
+
+        SeanceFormation seanceFormation2 = new SeanceFormation();
+        seanceFormation2.setDate("15/04/2021");
+        seanceFormation2.setHeureDebut("15h");
+        seanceFormation2.setHeureFin("17h");
+        seanceFormation2.setSalleFormation(salleFormation);
+
+        //====================================================
+        SalleFormation salleFormation3 = new SalleFormatiion();
+        salleFormation3.setId(1);
+        salleFormation3.setNom("Blue-Ray");
+        salleFormation3.capicite(30);
+
+        SeanceFormation seanceFormation3 = new SeanceFormation();
+        seanceFormation3.setDate("18/05/2021");
+        seanceFormation3.setHeureDebut("16h");
+        seanceFormation3.setHeureFin("18h");
+        seanceFormation3.setSalleFormation(salleFormation);
+
         //Ajout des séances de formation à une date
-        SeanceFormation [] tabSeances = new SeanceFormation();
+        SeanceFormation [] tabSeances = new SeanceFormation[5];
+        tabSeances[0] = seanceFormation1;
+        tabSeances[1] = seanceFormation2;
+        tabSeances[2] = seanceFormation3;
 
 
 
